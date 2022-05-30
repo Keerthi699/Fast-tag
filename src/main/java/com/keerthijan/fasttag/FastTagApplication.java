c7faeb284dcc20a739e87119c8b70778fdf061ee
keerthi <keerthij62@gmail.com>
2022-05-30 11:43:04 +0530

    Fasttag's initial commit

---
+++ src/main/java/com/keerthijan/fasttag/FastTagApplication.java
@@ -0,0 +1,13 @@
+package com.keerthijan.fasttag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastTagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastTagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keerthijan/fasttag/FastTagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keerthijan.fasttag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastTagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
